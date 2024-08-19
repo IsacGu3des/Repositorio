7d425fbb66191d0b8491b409cc370d486d70b3dc
IsacGu3des <isac.pinheiro@escolar.ifrn.edu.br>
2024-08-19 08:28:39 -0300

    salvei desta vez(Aula 5)

--- Projeto/Assets/Scripts/Player.cs
+++ Projeto/Assets/Scripts/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public float force;
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
         
     }
+    void Move()
+    {
+        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"),0f, 0f);
+        transform.position += movimento * Time.deltaTime * speed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0f, force), ForceMode2D.Impulse);
+        }
+    }
+
 }
